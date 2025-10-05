23982fb56eb01d2bd7cb6d1b85df10d1601188c4
AX <axie08@student.ubc.ca>
2025-10-05 09:49:47 -0700

    implemented writeDataset

--- dataset/utils/writeDataset.js
+++ dataset/utils/writeDataset.js
@@ -1,7 +1,34 @@
 const fs = require('fs-extra');
 
-function writeDataset() {
-  
+
+// content should be json string
+async function writeDataset(content) {
+    const dirPath = "./data";
+    const filePath = "./data/students.json";
+
+    try{
+        const newItem = JSON.parse(content);
+    } catch (err) {
+        console.error('Invalid JSON input:', err);
+    }
+    await fs.ensureDir(dirPath);
+
+
+    try {
+    // Ensure file exists and has valid JSON
+    const list = await fs.readJson(filePath).catch(() => []);
+
+    // Add new item
+    list.push(newItem);
+
+    // Write updated list back to file
+    await fs.writeJson(filePath, list, { spaces: 2 });
+
+    console.log('✅ Item added to list');
+  } catch (err) {
+    console.error('❌ Failed to update file:', err);
+  }
+
 }
 
 module.exports = { writeDataset };
